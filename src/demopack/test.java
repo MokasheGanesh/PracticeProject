cbd3955a878812f4e148d2c15e433929d2942a1e
KanchanMokashi <mokashi.kb@gmail.com>
2022-05-24 22:39:59 +0530

    Hello world program added

--- src/demopack/test.java
+++ src/demopack/test.java
@@ -1,49 +1,15 @@
 package demopack;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+
 
 public class test {
 
 	public static void main(String[] args) {
 		
 		
-	/*	int a=10;
-		int b=0;
-		int c=0;
-		int s[]= {1,2,3,4};
-				
-		
-	try {
-		
-		 	c=a/b;
-			 System.out.println(s[4]);
-			
-		}
-//	catch(ArithmeticException e)
-//		{
-//			System.out.println("b is zero");
-//		}
-//	catch(ArrayIndexOutOfBoundsException e2)
-//		{
-//		System.out.println("Last index is 3");
-//		}
-	
-		
-	catch(Exception e)
-	{
-		System.out.println("Exception handled");
-		e.printStackTrace();
-		e.getMessage();
-		e.toString();
-	}
-	
-	System.out.println(c);
-	*/
+	System.out.println("Hello");
 	
 	}
 }
