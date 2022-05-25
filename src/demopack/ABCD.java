280e47489da7ce564057600c70c44c4033098ea4
ganesh.mokashe2 <gbmokashe1997@gmail.com>
2022-05-25 10:11:10 +0530

    Added new file

---
+++ src/demopack/ABCD.java
@@ -0,0 +1,12 @@
+package demopack;
+
+public class ABCD {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("We are practicing");
+		
+		}
+
+}
--- src/demopack/test.java
+++ src/demopack/test.java
@@ -9,7 +9,7 @@ public class test {
 	public static void main(String[] args) {
 		
 		
-	System.out.println("Hello World");
+	System.out.println("Hello");
 	
 	}
 }
